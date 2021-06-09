00864ff9eaed496dd5b41adb26be5455129966d8
shadab91 <shadabali324@gmail.com>
2021-06-09 16:40:20 +0530

    Category change. Fixes #2.

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-       //My First Changes
+       //My Second Changes
         [Key]
         public int CartItemId { get; set; }
 
